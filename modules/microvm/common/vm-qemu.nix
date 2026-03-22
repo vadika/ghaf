ce7b6dd32e8c0a10f352194bcf068d362ea1652b
vadik likholetov <vadikas@gmail.com>
2026-03-22 09:58:47 +0000

    overlay hunt: refactor qemu_kvm from global overlay to module
    Move QEMU patches and ivshmem-server build from the global nixpkgs
    overlay into a new ghaf.virtualization.qemu.package module option.
    
    This eliminates a global override on pkgs.qemu_kvm that changed its
    derivation hash and forced rebuilds of all transitive dependents from
    source instead of using binary cache.
    
    All consumer modules (shared-mem, windows-launcher, bpmp-virt-host)
    now reference config.ghaf.virtualization.qemu.package. The bpmp-virt-host
    module's stray nixpkgs.overlays is also removed, resolving an existing
    TODO.
    
    Signed-off-by: vadik likholetov <vadikas@gmail.com>

---
+++ modules/microvm/common/vm-qemu.nix
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2022-2026 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# Wire the Ghaf-patched QEMU into microvm.nix so all VMs use it.
+#
+{ config, inputs, ... }:
+{
+  _file = ./vm-qemu.nix;
+
+  imports = [ inputs.self.nixosModules.ghaf-qemu ];
+
+  microvm.qemu.package = config.ghaf.virtualization.qemu.package;
+}
